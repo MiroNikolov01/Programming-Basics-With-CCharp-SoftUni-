d141b89f434cb99bdf3c191394136afb486392c8
Stanimir.Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2024-05-26 23:02:26 +0300

    Add files via upload

---
+++ HelloSoftUni.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Hello_SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
