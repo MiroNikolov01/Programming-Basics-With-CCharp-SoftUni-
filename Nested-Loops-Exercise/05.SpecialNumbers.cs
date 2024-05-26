9206c253ae82642b35652de8c94ccedacd808496
Stanimir.Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2024-05-27 00:51:52 +0300

    Update 05.SpecialNumbers.cs

--- Nested-Loops-Exercise/05.SpecialNumbers.cs
+++ Nested-Loops-Exercise/05.SpecialNumbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,16 +13,20 @@ namespace _05.SpecialNumbers
             int n = int.Parse(Console.ReadLine());
             for (int i = 1; i <= 9; i++)
             {
+                if (n % i > 0) continue;
                 for (int j = 1; j <= 9; j++)
                 {
+                    if (n % j > 0) continue;
                     for (int k = 1; k <= 9; k++)
                     {
+                        if (n % k > 0) continue;
                         for (int l = 1; l <= 9; l++)
                         {
-                            if (n % l == 0 && n % k == 0 && n % j == 0 && n % i == 0)
+                            if (n % l == 0) 
                             {
                                 Console.Write($"{i}{j}{k}{l} ");
                             }
+
                         }
                     }
                 }
