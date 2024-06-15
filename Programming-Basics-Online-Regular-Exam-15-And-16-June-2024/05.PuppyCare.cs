e3bb8a077d672391825d823cda0d382e2a741a84
Stanimir_Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2024-06-15 14:12:42 +0300

    Add files via upload

---
+++ Programming-Basics-Online-Regular-Exam-15-And-16-June-2024/05.PuppyCare.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalQuantityFood = int.Parse(Console.ReadLine()) * 1000;
+            string command = string.Empty;
+            int eatenFood = 0;
+            while ((command = Console.ReadLine()) != "Adopted")
+            {
+                 eatenFood += int.Parse(command);
+            }
+            if (totalQuantityFood >= eatenFood)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {totalQuantityFood - eatenFood} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {eatenFood - totalQuantityFood} grams more.");
+            }
+        }
+    }
+}
